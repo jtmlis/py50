d45df4106363db08a9da1d80441ef81ff41faf57
Julien Tremblay McLellan <jtremc@gmail.com>
2022-11-26 15:44:01 -0500

    test for 2 camel to snake case

--- camel/camel.py
+++ camel/camel.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # https://cs50.harvard.edu/python/2022/psets/2/camel/
 #
-
+# import ipdb
 # Takes user Str in camelCase
 # Converts it to  to snake_case
 def main():
+    # ipdb.set_trace()
     userString = str(input())
-    return (camelcase_converter(userString))
+    return camelcase_converter(userString)
 
 def camelcase_converter(userString: str):
+    snake_case = ""
     for char in userString:
         if char.isupper():
-            print("_"+char.lower(), end="")
+            snake_case += ("_"+char.lower())
         else:
-            print(char,sep="")
-
+            snake_case += (char)
+    return snake_case.strip(" ")
 # main()
