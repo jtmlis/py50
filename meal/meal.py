c0ca65e996be7fef4e214ac48624f86f7e3e1a5e
Julien Tremblay McLellan <jtremc@gmail.com>
2022-11-25 22:15:55 -0500

    code test working

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+#https://cs50.harvard.edu/python/2022/psets/1/meal/
+#
 def main():
     userTime = str(input("Enter the time"))
-
-def convert(time):
-    if time == "7:30":
-        return 7.5
-
+    return convert(userTime)
+def convert(userTime: str):
+    hour, minutes = userTime.split(":")
+    hour = float(hour)
+    minutes = float(minutes)
+    minutes = minutes / 60
+    return hour + minutes
 # if __name__ == "__main__" :
     # main()
