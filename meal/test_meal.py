2e3af1330b7c502b59249a62b61fd828dcc5815a
Julien Tremblay McLellan <jtremc@gmail.com>
2022-11-26 14:13:17 -0500

    add time intervals

--- meal/test_meal.py
+++ meal/test_meal.py
@@ -6,6 +6,7 @@ class TestConvert:
 
     def test_convert(self):
         assert convert("7:30") == 7.5
+        assert not convert("7:29") == 7.5
         assert convert("8:30") == 8.5
 
     def test_convert_am(self):
@@ -15,3 +16,7 @@ class TestConvert:
     def test_convert_pm(self):
         assert convert("7:30pm") == 19.5
         assert convert("8:30pm") == 20.5
+
+    def test_breakfest_time(self):
+        assert convert("7:00") == "breakfest time"
+        assert not convert("11:00") == "breakfast time"
