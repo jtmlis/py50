40a17218e54b8e0c868632c286fbd68490a40222
Julien Tremblay McLellan <jtremc@gmail.com>
2022-11-26 20:13:40 -0500

    test return true if 2 valid plates

--- plates/plates.py
+++ plates/plates.py
@@ -6,9 +6,20 @@ def main():
         print("Invalid")
 
 
-def is_valid(s:str):
-    if s == "AAA222":
-        return "valid"
-
+def is_valid(s):
+    n = [0,1,2,3,4,5,6,7,8,9]
+    """for char in string 0-3 return valid"""
+    part1 = s[0:2]
+    part2 = s[3:]
+    for c in part1:
+        if c not in n:
+            return True
+        else:
+            return False
+    for c in part2:
+        if c in n:
+           return True
+        else:
+           return False
 
 #main()
