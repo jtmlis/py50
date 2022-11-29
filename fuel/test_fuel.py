749af0314c8f88d0cd9e389b696ffd2ae8e4f352
Julien Tremblay McLellan <jtremc@gmail.com>
2022-11-28 21:23:21 -0500

    add test: pytest:red

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -7,6 +7,7 @@ from fuel import fuelgauge
 def test_fuelgauge():
     assert fuelgauge("3/4") == ("75%")
 # 1/4 outputs 25%
+    assert fuelgauge("1/4") == ("25%")
 # 4/4 outputs F
 # 0/4 outputs E
 # 4/0 outputs ZeroDivisonError
