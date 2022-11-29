0caf2cb4435fe12da6227569ae9e1c9e7b252697
Julien Tremblay McLellan <jtremc@gmail.com>
2022-11-28 20:53:04 -0500

    init fuel

---
+++ fuel/fuel.py
@@ -0,0 +1,12 @@
+#https://cs50.harvard.edu/python/2022/psets/3/fuel/
+
+# Takes x & y as INT
+
+# returns as a rounded % to nearest Int
+# If less than 1% return Empty
+# If greater than 99% return Full
+
+
+# if x or y not an INT prompt again
+# is X greater than Y
+# if y is 0
---
+++ fuel/test_fuel.py
@@ -0,0 +1,12 @@
+#https://cs50.harvard.edu/python/2022/psets/3/fuel/
+
+from fuel import *
+# Tests from website
+
+# "3/4" should output 75 %
+# 1/4 outputs 25%
+# 4/4 outputs F
+# 0/4 outputs E
+# 4/0 outputs ZeroDivisonError
+# three/four ouputs ValueError
+# 5/4 outputs prompt for user again
